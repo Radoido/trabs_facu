26ab4965c6227cc5788389477d8ea9f5ffb52894
leFael <rafagrb@live.com>
2024-04-23 17:00:24 -0300

    git commit -m "removendo comentarios"

--- estru_dados/Jogo_de_beisebol/Principal.java
+++ estru_dados/Jogo_de_beisebol/Principal.java
@@ -9,55 +9,48 @@ public class Principal {
 
         for ( int i=0; i < entrada.length(); i++ ){              
 
-           if (entrada.substring(i,i+1).equals("+")){
-                
-                int a = 0;
-                //pilha.push(pilha.top()*2);
-                a = pilha.pop();
-                val = pilha.top() + a;
-                pilha.push(a);
-                pilha.push(val);
-
-           
-            }else if (entrada.substring(i,i+1).equals("D")){
-                //pilha.push(pilha.top()*2);
-                val = pilha.top()*2;
-                pilha.push(val);
-                
-            }else if (entrada.substring(i,i+1).equals("C")){
-                pilha.pop();
-
-
-            }else if (entrada.substring(i,i+1).equals("-")){
-                pilha.push(-(Integer.parseInt(entrada.substring(i+1, i+2))));
-                i++;
+            if (entrada.substring(i,i+1).equals("+")){                
+                 int a = 0;
+ 
+                 a = pilha.pop();
+                 val = pilha.top() + a;
+                 pilha.push(a);
+                 pilha.push(val);
+ 
+             }else if (entrada.substring(i,i+1).equals("D")){
+ 
+                 val = pilha.top()*2;
+                 pilha.push(val);
+                 
+             }else if (entrada.substring(i,i+1).equals("C")){
+                 pilha.pop();
+ 
+             }else if (entrada.substring(i,i+1).equals("-")){
+                 pilha.push(-(Integer.parseInt(entrada.substring(i+1, i+2))));
+                 i++;
+             
+             }else if (entrada.substring(i,i+1).equals(",")){
+                int b = 0 ; 
+                i += b;
+             
+             }else{ 
+ 
+                 pilha.push(Integer.parseInt(entrada.substring(i, i+1)));
+             }
+         }
+ 
+         int a = 0;
+         val = pilha.size();
+ 
+         for (int i = 0; i < val; i++){
+             a += pilha.pop();       
+         }
+ 
+         System.out.println(a);
+ 
             
-            }else if (entrada.substring(i,i+1).equals(",")){
-               int b = 0 ; 
-               i += b;
-            
-            }else{
-
-                pilha.push(Integer.parseInt(entrada.substring(i, i+1)));
-
-                
-            }
-        }
-        int a = 0;
-        val = pilha.size();
-
-        for (int i = 0; i < val; i++){
-//            System.out.println("size" + pilha.size());
-            a += pilha.pop(); 
-            
-
-            
-        }
-
-        System.out.println(a);
-
-           
-    }
-}
-
-
+     }
+ }
+ 
+ 
+ 
