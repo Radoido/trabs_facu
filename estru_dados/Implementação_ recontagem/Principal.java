9f62bbc01af355ced044f159f60bc563a7ad4faf
leFael <rafagrb@live.com>
2024-06-12 12:34:56 -0300

    ajustando css

--- estru_dados/Implementação_ recontagem/Principal.java
+++ estru_dados/Implementação_ recontagem/Principal.java
@@ -6,65 +6,23 @@ public class Principal {
 
   public static void main(String[] args) {
     String entrada = scanner.nextLine();
-    Dict<String, Integer> map = new SortedArrayDict<>();
+    Dict<String, Integer> contadorDeNomes = new SortedArrayDict<>();
+    String[] nomes = entrada.split(",");
 
-    String nomes[] = entrada.split(",");
-    String[] lista = new String[nomes.length];
+    for (int i = 0; i < nomes.length; i++) {
+      contadorDeNomes.put(nomes[i], i);
 
+      if (nomes[i] == contadorDeNomes.toString()) {
 
-      //verifica quantas vezes um nome é repetido 
-      for (int i = 0; i < nomes.length; i++) {
-        int x = 0;
-        for (int j = 0; j < nomes.length; j++) {
-          if (nomes[i].equals(nomes[j])) {
-
-            x++;
-
-          }
-
-        }
-        map.put(nomes[i], x);
       }
+
     }
+    System.out.println(contadorDeNomes.toString());
   }
 }
 
+// Verifica se há um empate
+
 /*
- * String vencedor = "";
- * int empate = 0;
- * int maior = 0;
- * 
- * for (int i = 0; i < nomes.length; i++) {
- * for (int j = 0; j < nomes.length; j++) {
- * if (map.get(nomes[i]) > (map.get(nomes[j]))) {
- * maior = map.get(nomes[i]);
- * vencedor = nomes[i];
- * System.out.println("maior: "+maior);
- * 
- * 
- * 
- * } else if (map.get(nomes[i]) == (map.get(nomes[j])) && nomes[i] != nomes[j]){
- * empate = map.get(nomes[i]);
- * System.out.println("empate: " + nomes[i] + " "+ nomes[j]);
- * }
- * 
- * }
- * 
- * }
- * if (empate >= maior){
- * System.out.println("Segundo turno!");
- * 
- * }else{
- * System.out.println(vencedor);
- * }
- * 
- * 
- * 
- * 
- * 
- * 
- * }
- * }}
- * /*
  * Ana,Pedro,Ana,Paulo,Ana,Ana,Ana,Ana,Paulo,Vicente,Pedro
  */
